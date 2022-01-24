4e4141dcb4cd587219108043054db554edaf2bec
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-25 01:03:06 +0600

    redux persist added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { persistor, store } from "./redux/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactSlice from './slice/contactSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import contactSlice from "./slice/contactSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactSlice);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactSlice
+    contacts: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
